9f074da7b54419f67d5fcfd3da7391bc5b45add4
sarahcelda <40496531@live.napier.ac.uk>
2024-08-24 16:47:02 +0100

    add nullable symbol to attribitues/variables

--- UniCollabMaui/Models/User.cs
+++ UniCollabMaui/Models/User.cs
@@ -12,9 +12,9 @@ namespace UniCollabMaui.Models
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string ?Name { get; set; }
+        public string ?Username { get; set; }
+        public string ?Password { get; set; }
         public int RoleId { get; set; }    
     }
 }
