1f56dc534d2f5f39298f3ba06cf436e0249e9585
sarahcelda <40496531@live.napier.ac.uk>
2024-11-04 19:12:45 +0000

    v

---
+++ UniCollab_Tests/LogInViewTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.Maui.Controls;
+using Moq;
+using System.Threading.Tasks;
+using UniCollabMaui.Views;
+using UniCollabMaui.Service;
+using Xunit;
+
+namespace UniCollab_Tests
+{
+    public class LogInViewTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
